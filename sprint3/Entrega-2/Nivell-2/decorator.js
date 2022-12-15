08395865d1f87394adeca62ab95a3542b7fd48ca
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-15 11:10:43 +0100

    canvis decorator

--- sprint3/Entrega-2/Nivell-2/decorator.js
+++ sprint3/Entrega-2/Nivell-2/decorator.js
@@ -1,17 +1,16 @@
 /* Crea un Decorator en un arxiu que retorni una funció. Aquesta funció efectuarà una conversió de moneda a euros multiplicant pel coeficient de conversió del fitxer adjunt currency_conversions.json en funció de la divisa original. */
 
-
-const decorator = ( article ) => {
-    const conversions = require( './currency_conversions.json' );
-    const convertirDivisa = `${ article.divisa }_EUR`;
-    const preuArrodonit = Math.round( article.preu * conversions[ convertirDivisa ] * 100 ) / 100;
-    return ( () => ({
-        nom: article.nom,
-        preu: preuArrodonit,
-        divisa: 'EUR'
-    }))();
+const decorator = (article) => {
+  const conversions = require('./currency_conversions.json');
+  const convertirDivisa = `${article.divisa}_EUR`;
+  const preuArrodonit = Math.round(article.preu * conversions[convertirDivisa] * 100) / 100;
+  return (() => [
+    article.nom,
+    article.preu,
+    article.divisa,
+    article.preuEnEuros = preuArrodonit,
+    'EUR'
+  ])();
 };
-
-
 
 module.exports = decorator;
